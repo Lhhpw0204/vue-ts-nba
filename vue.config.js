071f0144499837ab7d6c9bcec84812d06b744560
simon.liu <liuhenghui0902@outlook.com>
2019-11-21 23:01:59 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'https://slamdunk.sports.sina.com.cn',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    },
+    lintOnSave: true,
+    css: {
+        extract: true,
+        sourceMap: false,
+        loaderOptions: {
+            sass: {
+                prependData: `@import "./assets/style/common.scss";`
+            }
+        }
+    }
+}
